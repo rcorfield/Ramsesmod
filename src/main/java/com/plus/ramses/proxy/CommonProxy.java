75440886a45b2d698748d723a1d088762b2753db
Richard Corfield <rcorfield@users.noreply.github.com>
2014-12-22 22:26:10 +0000

    Ramses Minecraft Mod
    Initial commit of an embryonic Minecraft Mod

---
+++ src/main/java/com/plus/ramses/proxy/CommonProxy.java
@@ -0,0 +1,9 @@
+package com.plus.ramses.proxy;
+
+public class CommonProxy {
+
+        // Client stuff
+        public void registerRenderers() {
+                // Nothing here as the server doesn't render graphics or entities!
+        }
+}
